b72f090044f8d3c13feb419fe2bfe5a5b05c8409
akula12345 <90633062+akula12345@users.noreply.github.com>
2021-09-27 16:18:29 +0300

    add develop branch

---
+++ EmptyProject/EmptyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmptyProject
+//
+//  Created by Вячеслав on 27.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
