a49fe67c149b351f3ca8f272a0ee355e74bd58f3
akula12345 <90633062+akula12345@users.noreply.github.com>
2021-10-06 20:34:37 +0300

    add task

---
+++ EmptyProject/LearningViewController/ViewControllers/ThirdViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ThirdViewController.swift
+//  LearningViewController
+//
+//  Created by Вячеслав on 04.10.2021.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func closeButtonTouchUpInsde(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ EmptyProject/LearningViewController/ViewControllers/ViewController4.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController4.swift
+//  LearningViewController
+//
+//  Created by Вячеслав on 04.10.2021.
+//
+
+import UIKit
+
+class ViewController4: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func goToFifthViewController(_ sender: UIButton) {
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let vc = main.instantiateViewController(withIdentifier: "Fifth View Controller") as! ViewController5
+        vc.title = "Fifth View Controller"
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
